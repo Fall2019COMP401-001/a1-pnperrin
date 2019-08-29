e25973b22316c82241e44d863acb9e56578ed153
BuildTools <unconfigured@null.spigotmc.org>
2019-08-29 14:39:20 -0400

    Finished A1Adept and A1Novice

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -1,14 +1,132 @@
 package a1;
 
+//import java.io.File;
+//import java.io.FileNotFoundException;
+import java.text.DecimalFormat;
+import java.util.HashMap;
 import java.util.Scanner;
+
+
+import com.sun.javafx.collections.MappingChange.Map;
 
 public class A1Adept {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { //throws FileNotFoundException {
 		
+		
+		//File text = new File("src/a1/text.txt");
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
 		
+		//create hashmap
+		HashMap<String, Double> map = new HashMap<String, Double>();
+		
+		//declare variables
+		int count_items;
+		int count_people;
+		int count_purchase;
+		
+		count_items = scan.nextInt();
+		
+		//put values for items and their price in map
+		for (int i = 0; i < count_items; i++) {
+			map.put(scan.next(), scan.nextDouble());
+		}
+		
+		count_people = scan.nextInt();
+		
+		//declare names and totals arrays
+		double[] totals = new double[count_people];
+		String[] first_n = new String[count_people];
+		String[] last_n = new String[count_people];
+		
+		//put names/last names and totals in respective arrays
+		for (int i = 0; i < count_people; i++) {
+			//Person's names and number of different types of items purchased
+			first_n[i] = scan.next();
+			last_n[i] = scan.next();
+			count_purchase = scan.nextInt();
+					
+			//calculate total paid for purchase
+			for (int j = 0; j < count_purchase; j++) {
+				totals[i] += scan.nextInt()*map.get(scan.next());
+			}
+		}
+		//print highest/lowest/average as desired output
+		getHighest(totals, first_n, last_n);
+		getLowest(totals, first_n, last_n);
+		getAverage(totals);
+	}
+	
+	/////////////////////////////////////////////
+
+	/* VOID METHODS */
+	
+	//find the max amount
+	private static void getHighest(double[] totals, String[] first_n, String[] last_n) {
+		
+		//format decimal
+		DecimalFormat df = new DecimalFormat("##.00");
+
+		//local var
+		String name;
+		double max = totals[0];
+		int max_index = 0;
+		
+		//find max
+		for(int i = 0; i < totals.length; i++) {
+			if(totals[i] > max) {
+				max = totals[i];
+				max_index = i;
+			}
+		}
+		//void output
+		System.out.println("Biggest: " + first_n[max_index] + " " + last_n[max_index] + " (" + df.format(max) +")");
+	}
+	
+	/////////////////////////////////////////////
+	
+	//find the min amount
+	private static void getLowest(double[] totals, String[] first_n, String[] last_n) {
+		
+		//format decimal
+		DecimalFormat df = new DecimalFormat("##.00");
+		
+		//local var
+		String name;
+		double min = totals[0];
+		int min_index = 0;
+		
+		//find min
+		for(int i = 0; i < totals.length; i++) {
+			if(totals[i] < min) {
+				min = totals[i];
+				min_index = i;
+			}
+		}
+		//void output
+		System.out.println("Lowest: " + first_n[min_index] + " " + last_n[min_index] + " (" + df.format(min) +")");
+	}
+	
+	/////////////////////////////////////////////
+
+	//calculate average
+	private static void getAverage(double[] totals) {
+		
+		//format decimal
+		DecimalFormat df = new DecimalFormat("##.00");
+		
+		//local var
+		double total_spent = 0;
+		double average;
+		
+		//calculate total money spent between all customers
+		for(int i = 0; i < totals.length; i++)
+			total_spent += totals[i];
+		
+		
+		average = total_spent/(double)totals.length;
+		
+		//void output
+		System.out.println("AVERAGE: " + df.format(average));
 	}
 }
--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,45 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
 		
-	}
+		//declare variables
+		String nope;
+		int count;
+		int number_items;
+		double cost;
+		double quantity;
+		double check;
+										
+		// count number of customers
+		count = scan.nextInt();
+			
+		//make cost/first name/last name arrays from the number of customers
+		double[] totals = new double[count];
+		String[] first_n = new String[count];
+		String[] last_n = new String[count];
+				
+		//intitialize all totals to 0
+		for(int i = 0; i < count; i++)
+			totals[i] = 0;
+		
+		//fill arrays
+		for(int i = 0; i < count; i++) {
+			first_n[i] = scan.next();
+			last_n[i] = scan.next();
+			number_items = scan.nextInt();
+			
+			for(int j = 0; j < number_items; j++) {
+				quantity = scan.nextInt();
+				nope = scan.next();
+				cost = scan.nextDouble();
+				totals[i] += quantity*cost;
+				}
+			}
+				
+		for(int i = 0; i < count; i++)
+			System.out.println(first_n[i].charAt(0)+". " + last_n[i] + ": " + totals[i]);
+		
+		scan.close();
+		
+	}		
 }
