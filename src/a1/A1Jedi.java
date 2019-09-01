c2d62d4fd9a8425b72e92cf93dff87a4a659e277
BuildTools <unconfigured@null.spigotmc.org>
2019-09-01 12:18:32 -0400

    Initial Commit A1Jedi

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -1,14 +1,74 @@
 package a1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class A1Jedi {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { // throws FileNotFoundException {
 		
+		//File text = new File("src/a1/text.txt");
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+				
+		//create hashmap
+		//HashMap<String, Double> map = new HashMap<String, Double>();
 		
+		//declare variables
+		int count_items;
+		int count_people;
+		int count_purchase;
+		double tmp;
+		String nope_l;
+		String nope_f;
+		String item;
+		int quantity;
+		
+		//get the number of items
+		count_items = scan.nextInt();
+		
+		//make an array for the items/number of customers/number of items bought
+		String[] items = new String[count_items];
+		int[] number_customer = new int[count_items];
+		int[] number_items = new int[count_items];
+		
+		//put items in array
+		for (int i = 0; i < count_items; i++) {
+			items[i] = scan.next();
+			tmp = scan.nextDouble();
+		}
+		
+		//loop through number of customers and their purchase
+		count_people = scan.nextInt();
+		
+		//for the number of customers roll through purchase
+		for (int i = 0; i < count_people; i++) {
+			nope_f = scan.next();
+			nope_l = scan.next();
+			
+			count_purchase = scan.nextInt();
+			
+			for (int j = 0; j < count_purchase; j++) {
+				
+				quantity = scan.nextInt();
+				
+				item = scan.next();
+				for (int z = 0; z < items.length; z++) {
+					if(items[z].equals(item)) {
+						number_customer[z]++;
+						number_items[z] += quantity;
+					}
+				}
+			}
+		}
+		for(int i = 0; i < items.length; i++) {
+			if(number_customer[i] == 0) {
+				System.out.println("No customers bought " + items[i]);
+			} else {
+				System.out.println(number_customer[i] + " customers bought " + number_items[i] + " " + items[i]);
+			}
+		}
 	}
 }
